bd65538f7daf744ceb8d8e0b6767ce107ed778ac
Sebastian López Reyes <sebastian-lopez77@hotmail.com>
2016-05-18 13:17:44 -0700

    Primeros archivos

---
+++ supertaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  supertaps
+//
+//  Created by Sebastian Lopez on 18/5/16.
+//  Copyright © 2016 Sebastian Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
